e0c45e961381fb89334379b7e88f85dfefe22c01
Allan Miljan <147393230+TaaviAllingu@users.noreply.github.com>
2026-03-06 11:38:55 +0200

    working alpha

--- scripts.js
+++ scripts.js
@@ -1,24 +1,33 @@
 const sendbtn = document.querySelector('#send')
 const letter = document.querySelector('input')
 const log = document.querySelector('div')
-const message = {user: 'rafael', message: 'hello everyone'}
+
 
 
 async function send(event) {
-    const send_db = await fetch('https://tinkr.tech/sdb/rafael_chat', {
+    const message = {user: 'rafael', message: letter.value}
+    const send_db = await fetch('https://tinkr.tech/sdb/rafael_chat_final_test', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(message)
   });
   const send_out = await send_db.json();
-  console.log(send)
+  console.log(send_out)
   message_log ()
 }
 
-async function message_log () {
-  const response = await fetch('https://tinkr.tech/sdb/rafael_chat');
+async function message_log() {
+  const response = await fetch('https://tinkr.tech/sdb/rafael_chat_final_test');
   const documents = await response.json();
-  console.log(documents)
+  
+  log.innerHTML = ''; 
+  letter.value = ''; 
+
+  documents.forEach(doc => {
+    const messageElement = document.createElement('p');
+    messageElement.innerHTML = `<strong>${doc.user}:</strong> ${doc.message}`;
+    log.appendChild(messageElement);
+  });
 }
 
 sendbtn.addEventListener("click", send)
